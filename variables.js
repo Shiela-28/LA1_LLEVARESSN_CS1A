4c468d12594419fd6d6f8d6833fea9f98a6f3e7f
Shiela N. Llevares <llevaresshiela28@gmail.com>
2025-03-06 19:18:24 +0800

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,11 @@
+let name = "Your Name Here";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Your Gender Here";
+let twitterHandle = "YourTwitterHandle";
+
+console.log("Name:", name);
+console.log("Future Profession:", whatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+if (twitterHandle) {
+  console.log("Twitter Handle:", twitterHandle);
+}
